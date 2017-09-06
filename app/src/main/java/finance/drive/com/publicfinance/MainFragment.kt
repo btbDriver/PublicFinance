c2c911bd07528aea15f95d9cf1b41317c5ada6aa
Liu Chao <liuchao5@xiaomi.com>
2017-09-06 14:27:10 +0800

    添加主页面UI

--- app/src/main/java/finance/drive/com/publicfinance/MainFragment.kt
+++ app/src/main/java/finance/drive/com/publicfinance/MainFragment.kt
@@ -4,16 +4,45 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import finance.drive.com.publicfinance.base.BaseFragment
+import android.widget.ImageView.ScaleType
+import com.jude.rollviewpager.RollPagerView
+import com.jude.rollviewpager.adapter.LoopPagerAdapter
+
+
 
 class MainFragment : BaseFragment() {
 
+    val rollPagerView by lazy {
+        view?.findViewById(R.id.rollPagerView) as RollPagerView
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        rollPagerView.setAdapter(TestLoopAdapter(rollPagerView))
+    }
+}
+
+internal class TestLoopAdapter(viewPager: RollPagerView) : LoopPagerAdapter(viewPager) {
+    private val imgs = intArrayOf(R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher, R.mipmap.ic_launcher)
+
+    override fun getView(container: ViewGroup, position: Int): View {
+        val view = ImageView(container.context)
+        view.setImageResource(imgs[position])
+        view.setScaleType(ImageView.ScaleType.CENTER_CROP)
+        view.setLayoutParams(ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+        return view
+    }
+
+    override fun getRealCount(): Int {
+        return imgs.size
+    }
 }
 
 fun createMainFragment() : MainFragment {
