101dd3e7a2d51133b12de789cdc24e112c5fb0a5
Liu Chao <liuchao5@xiaomi.com>
2017-09-06 16:08:48 +0800

    添加我的投资UI

--- app/src/main/java/finance/drive/com/publicfinance/MineFragment.kt
+++ app/src/main/java/finance/drive/com/publicfinance/MineFragment.kt
@@ -1,19 +1,48 @@
 package finance.drive.com.publicfinance
 
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import finance.drive.com.publicfinance.base.BaseFragment
 
 class MineFragment : BaseFragment() {
+
+    val recyclerView by lazy {
+        view?.findViewById(R.id.recyclerView) as RecyclerView
+    }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_mine, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = MineListAdapter()
+    }
 }
+
+class MineListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        val rootView = LayoutInflater.from(parent!!.context).inflate(R.layout.fragment_mine_item, null, false)
+        rootView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        return MineListViewHolder(rootView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+}
+
+class MineListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
 fun createMineFragment(): MineFragment {
     return MineFragment()
