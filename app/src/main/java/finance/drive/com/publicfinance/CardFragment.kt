6bfbddf0f2b6ea791549299f3fc1255e4ad09e4d
Liu Chao <liuchao5@xiaomi.com>
2017-09-07 09:54:06 +0800

    添加众卡贷UI

--- app/src/main/java/finance/drive/com/publicfinance/CardFragment.kt
+++ app/src/main/java/finance/drive/com/publicfinance/CardFragment.kt
@@ -1,6 +1,11 @@
 package finance.drive.com.publicfinance
 
 import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+import android.support.v4.view.ViewPager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +13,40 @@ import finance.drive.com.publicfinance.base.BaseFragment
 
 class CardFragment : BaseFragment() {
 
+    val cardTabLayout by lazy {
+        view?.findViewById(R.id.cardTabLayout) as TabLayout
+    }
+    val viewPager by lazy {
+        view?.findViewById(R.id.viewPager) as ViewPager
+    }
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_card, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        cardTabLayout.setupWithViewPager(viewPager)
+        viewPager.adapter = CardPageAdapter(childFragmentManager)
+    }
+}
+
+class CardPageAdapter(fragmentManager: FragmentManager) : FragmentStatePagerAdapter(fragmentManager) {
+
+    val titles = arrayOf("关于众卡贷", "我的众卡贷")
+
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment {
+        if (position == 0) {
+            return createCardAboutFragment()
+        } else {
+            return createCardListFragment()
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence = titles[position]
 }
 
 fun createCardFragment() : CardFragment {
---
+++ app/src/main/java/finance/drive/com/publicfinance/CardListFragment.kt
@@ -0,0 +1,50 @@
+package finance.drive.com.publicfinance
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+
+
+class CardListFragment : Fragment() {
+
+    val recyclerView by lazy {
+        view?.findViewById(R.id.recyclerView) as RecyclerView
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_card_list, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = CardListAdapter()
+    }
+}
+
+class CardListAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecyclerView.ViewHolder {
+        val rootView = LayoutInflater.from(parent!!.context).inflate(R.layout.fragment_card_item, null, false)
+        rootView.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        return CardListViewHolder(rootView)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder?, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 10
+    }
+}
+
+class CardListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+fun createCardListFragment() : CardListFragment {
+    return CardListFragment()
+}
